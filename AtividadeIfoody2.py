f109438bc32b0ef27fdec824367f6f3513aec337
Alexandre Carvalho <alexandrealvesdecarvalho2@gmail.com>
2021-04-26 22:54:02 -0300

    Update AtividadeIfoody2.py

--- AtividadeIfoody2.py
+++ AtividadeIfoody2.py
@@ -7,22 +7,24 @@ Viena = [4.4,12.49,"Viena"]
 
 notas = [makisPlace,kibonSorveteriaSaúde, Sukiya, Giraffas, aFeijoada, Viena]
 
-notasParaOrdem = [] 
-for x in notas:          # criei uma nova lista e negativei o frete para ordem ficar certa.
-    x = [x[0],-x[1],x[2]]
-    notasParaOrdem.append(x)
-    
-Classificacao = []       
-for x in notasParaOrdem:
-    if Classificacao == [] or x < Classificacao[-1]:
-        Classificacao.append(x)
+notasParaOrdem = []                             #Criei uma lista nova para negativar o frete
+for x in notas:                                 # Para cada elemento em notas
+    x = [x[0],-x[1],x[2]]                       # iguala o elemento ao elemento do indice[0], -indice[1], indice[2]                      
+    notasParaOrdem.append(x)                    # Adiciona os elementos na lista notasParaOrdem
+
+
+
+Classificacao = []                                          #Criei uma nova lista para colocar em ordem
+for x in notasParaOrdem:                                    # para cada elemento em notasParaOrdem
+    if Classificacao == [] or x < Classificacao[-1]:        # se a lista classificacao estiver vazia ou se o elemento for menor que o ultimo elemento da lista
+            Classificacao.append(x)                         # Vai adicionar o elemento por ultimo na lista
     else:
-        pos = 0 
-        while pos < len(Classificacao):
-            if x > Classificacao[pos]:
-                Classificacao.insert(pos, x)
+        pos = 0                                             #contador do indice
+        while pos < len(Classificacao):                     # enquanto pos for menor que o tamanho da lista 
+            if x > Classificacao[pos]:                      # se o elemento for maior que a Classificacao com indice 0, vai inserir o elemento
+                Classificacao.insert(pos, x)                                 
                 break
-            pos += 1
+            pos += 1                                        # depois que achar o indice certo do elemento, vai adicionar +1 no contador pos, e usar a lógica novamenta até inserir todos os elementos
 
 cont=1
 for x in Classificacao:
